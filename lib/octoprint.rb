dc50d9f1c4555a57ac70287c19572c2c2c64ac80
Sophie Déziel <courrier@sophiedeziel.com>
2022-03-05 16:40:30 -0500

    update the documentation

--- lib/octoprint.rb
+++ lib/octoprint.rb
@@ -6,6 +6,7 @@ require "octoprint/client"
 # Welcome to the Octoprint Gem!
 #
 # This gem is still under development.
+# @attr [Octoprint::Client] client      The configured API client
 module Octoprint
   class Error < StandardError; end
   class AuthenticationError < Error; end
@@ -15,6 +16,10 @@ module Octoprint
     attr_reader :client
 
     # Configure the API client with the server's address and key
+    #
+    # @param [String] host      Server's API version
+    # @param [String] api_key   Server's version
+    # @return [true]
     def configure(host:, api_key:)
       @client = Client.new(host: host, api_key: api_key)
 
