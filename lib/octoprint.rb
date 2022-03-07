2f895800a17e2361896342228953cfbf59ef87b5
Sophie Déziel <courrier@sophiedeziel.com>
2022-03-07 16:55:14 -0500

    Threaded config

--- lib/octoprint.rb
+++ lib/octoprint.rb
@@ -19,17 +19,23 @@ require "octoprint/server_version"
 # @attr [Octoprint::Client] client      The configured API client
 module Octoprint
   class << self
-    attr_accessor :client
-
     # Configure the API client with the server's address and key
     #
     # @param [String] host      Server's API version
     # @param [String] api_key   Server's version
     # @return [true]
     def configure(host:, api_key:)
-      @client = Client.new(host: host, api_key: api_key)
+      self.client = Client.new(host: host, api_key: api_key)
 
       true
+    end
+
+    def client
+      Thread.current[:client]
+    end
+
+    def client=(client)
+      Thread.current[:client] = client
     end
   end
 end
