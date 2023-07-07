7b353b78de0156d72c7395fbc6a4758ee81ef9b6
Sophie Déziel <courrier@sophiedeziel.com>
2023-07-06 23:02:52 -0400

    rubocop fixes

--- spec/octoprint/client_spec.rb
+++ spec/octoprint/client_spec.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 RSpec.describe Octoprint::Client do
-  include_context "Octoprint config"
+  include_context "with default Octoprint config"
 
   it "raises when host is missing" do
     expect do
-      Octoprint::Client.new(host: nil, api_key: api_key)
+      described_class.new(host: nil, api_key: api_key)
     end.to raise_error Octoprint::Exceptions::MissingCredentials
   end
 
   it "raises when api_key is missing" do
-    expect { Octoprint::Client.new(host: host, api_key: nil) }.to raise_error Octoprint::Exceptions::MissingCredentials
+    expect { described_class.new(host: host, api_key: nil) }.to raise_error Octoprint::Exceptions::MissingCredentials
   end
 
   describe "#request" do
     let(:client) do
-      Octoprint::Client.new(host: host, api_key: api_key)
+      described_class.new(host: host, api_key: api_key)
     end
 
     it "returns the body as a hash", vcr: { cassette_name: "currentuser" } do
@@ -25,7 +25,7 @@ RSpec.describe Octoprint::Client do
     end
 
     it "raises when api_key is invalid", vcr: { cassette_name: "job:unauthenticated" } do
-      client = Octoprint::Client.new(host: host, api_key: "invalid")
+      client = described_class.new(host: host, api_key: "invalid")
       action = -> { client.request("/api/job") }
       expect(&action).to raise_error Octoprint::Exceptions::AuthenticationError
     end
@@ -33,7 +33,7 @@ RSpec.describe Octoprint::Client do
 
   describe "#use" do
     let(:client) do
-      Octoprint::Client.new(host: host, api_key: api_key)
+      described_class.new(host: host, api_key: api_key)
     end
 
     before do
@@ -54,9 +54,8 @@ RSpec.describe Octoprint::Client do
     end
 
     it "sets back the configured client after the block after exceptions" do
-      expect do
-        client.use { raise StandardError }
-      end.to raise_error StandardError
+      client.use { raise StandardError }
+    rescue StandardError
       expect(Octoprint.client.host).to eq "http://192.168.0.1"
     end
 
